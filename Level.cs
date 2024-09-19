5e827a471fc42d8706c653f0a97b7a403e3b0c8f
Flo <flo@uwap.org>
2024-09-19 17:44:08 +0200

    Added code for dynamic redrawing.

--- Level.cs
+++ Level.cs
@@ -6,6 +6,8 @@ public class Level
 
     public int Height;
 
+    public int CursorOffsetY;
+
     public List<Thing>[,] Fields;
 
     public Func<ConsoleKey,bool> KeyFunction = key => true;
@@ -15,6 +17,49 @@ public class Level
         Width = width;
         Height = height;
         Fields = fields;
+        CursorOffsetY = 0;
+    }
+
+    public void RedrawField(int x, int y)
+    {
+        Console.CursorLeft = x*2;
+        Console.CursorTop += y - CursorOffsetY;
+        CursorOffsetY = y;
+        DrawField(x, y);
+        Console.ResetColor();
+    }
+
+    public void ResetCursor()
+    {
+        Console.CursorLeft = 0;
+        Console.CursorTop += Height - CursorOffsetY;
+        CursorOffsetY = Height;
+    }
+
+    private void DrawField(int x, int y)
+    {
+        List<Thing> things = Fields[x, y];
+        ConsoleColor? background = null;
+        Content? content = null;
+        foreach (Thing thing in things)
+        {
+            if (thing.BackgroundColor != null)
+                background = thing.BackgroundColor;
+            if (thing.Content != null)
+                content = thing.Content;
+        }
+        if (background == null || content == null)
+            Console.ResetColor();
+
+        if (background != null)
+            Console.BackgroundColor = background.Value;
+            
+        if (content != null)
+        {
+            Console.ForegroundColor = content.Color;
+            Console.Write(content.Text);
+        }
+        else Console.Write("  ");
     }
 
     public void PrintToConsole()
@@ -23,33 +68,24 @@ public class Level
         {
             for (int x = 0; x < Width; x++)
             {
-                List<Thing> things = Fields[x, y];
-                if (things.Count == 0)
-                {
-                    Console.ResetColor();
-                }
-                else
-                {
-                    Console.BackgroundColor = things.Last().Color;
-                }
-                Console.Write("  ");
+                DrawField(x, y);
             }
             
             Console.ResetColor();
             Console.WriteLine();
         }
+        CursorOffsetY = Height;
     }
 
     public void Run()
     {
+        PrintToConsole();
         while (true)
         {
-            PrintToConsole();
             if (KeyFunction(Console.ReadKey(true).Key))
             {
                 return;
             }
-            Console.CursorTop -= Height;
         }
     }
 }
