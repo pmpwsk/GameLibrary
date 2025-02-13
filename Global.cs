93c873a8bb4d6d8d289318313cb945cf81d33195
pmpwsk <flo@uwap.org>
2025-02-13 18:20:41 +0100

    Added a global timer.

--- Global.cs
+++ Global.cs
@@ -1,6 +1,10 @@
+using System.Diagnostics;
+
 namespace uwap.GameLibrary;
 
 public static class Global
 {
     public static ReaderWriterLockSlim ConsoleLock { get; set; } = new();
+
+    public static Stopwatch GameTime = new();
 }
